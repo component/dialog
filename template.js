afef0ef7a7d815b47ac9234826b7163f32e4497b
TJ Holowaychuk <tj@vision-media.ca>
2012-09-21 11:50:46 -0700

    add hidden "close" em within .close
    we need this :D

--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<div id="dialog" class="hide">\n  <div class="content">\n    <span class="title">Title</span>\n    <a href="#" class="close">×</a>\n    <div class="body">\n      <p>Message</p>\n    </div>\n  </div>\n</div>';
+module.exports = '<div id="dialog" class="hide">\n  <div class="content">\n    <span class="title">Title</span>\n    <a href="#" class="close">× <em>close</em></a>\n    <div class="body">\n      <p>Message</p>\n    </div>\n  </div>\n</div>';
